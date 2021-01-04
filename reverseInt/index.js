8c5d705ca36a2a2fb99994ae65f16d916487e814
Edddo123 <edo.miraqian@gmail.com>
2021-01-04 04:15:21 +0400

    reverseString

---
+++ reverseInt/index.js
@@ -0,0 +1,46 @@
+//first try not ideal to say the least lol
+function reverseInt(n) {
+    if (n >= 0) {
+        let reverse = n.toString()
+        let arr = reverse.split('')
+        console.log(arr)
+        if (arr.length == 1) {
+            let output = arr[0]
+            return parseInt(output)
+        }
+        else {
+            let output = ''
+            for (let i = 0; i < arr.length; i++) {
+                if (arr[i] == 0) {
+                    arr[i] = ''
+                }
+                output = arr[i] + output
+            }
+            return parseInt(output)
+        }
+    }
+    else {
+        let reverse = n.toString()
+        let arr = reverse.split('')
+        let output = ''
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i] == 0) {
+                arr[i] = ''
+            }
+            output = arr[i] + output
+        }
+        let newArr = output.split('')
+        newArr.pop()
+        newArr.unshift('-')
+        let final = newArr.join('')
+        return parseInt(final)
+    }
+
+}
+
+
+//2nd try
+function reverseInt2(n) {
+    const reversed = n.split('').reverse().join('')
+    return parseInt(reverse) * Math.sign(n) //if n>=0 it returns 1 otherwise -1
+}
